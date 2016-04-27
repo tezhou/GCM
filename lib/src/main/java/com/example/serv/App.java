9e394446c789feab6e576981fd7c0f87a8fd8315
Teng Zhou <tezhou@UCSD.EDU>
2016-04-27 12:35:56 -0700

    gcm

--- lib/src/main/java/com/example/serv/App.java
+++ lib/src/main/java/com/example/serv/App.java
@@ -10,7 +10,7 @@ public class App {
 
         System.out.println("Sending POST to GCM");
 
-        String apiKey = "AIzaSyC_oi5v-QwyHirz_xbDzwcdcAROWrZ-xwc";
+        String apiKey = "AIzaSyCS76K_ygP_Se8kvBanF0aXbLXg9qyDGF8";
         Content content = createContent();
 
         Post2Gcm.post(apiKey, content);
@@ -19,7 +19,7 @@ public class App {
     public static Content createContent() {
         Content c = new Content();
 
-        c.addRegId("APA91bGB-ZbeyCtLP_QRNQE5zu4Y-1LgRlTFiIMh2vDrhtgVwL5vTo5DYiE_YFo4ftOjxk4Nihykt_LSTPL9T_7FX64I7EQoe-l_eQ_4-T36BwAdOgAioktNUE1fwFoaPe6fdzQ1Aayx");
+        c.addRegId(" APA91bHG1FYVbG4SYlen98t4WVUHw7cU5gqlmvxUzscEOi9HVdhQPagKI7EUoNF1TYw1vgiEuevLcPAMXmL8iPyKEd0IxGbW_ce_ddeIZ4faXq1RGNbm25Oq6rVUayx5t93OxSDRgMCm");
 
         c.createData("Working!!", "Test message");
 
